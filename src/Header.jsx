e28ca0f31aa12729d8c9f5ca573a4d557fb84f7d
nishant <nishantd28satere@gmail.com>
2023-06-05 22:57:28 +0530

    Created Navbar

---
+++ src/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import './Header.css'
+import amazonlogo from './amazon-logo.png'
+import { SearchRounded } from "@mui/icons-material";
+import { ShoppingBasket } from "@mui/icons-material";
+
+const Header = () => {
+    return <div className="header">
+
+        <img className="header-logo"
+            src={amazonlogo} 
+        />
+
+        <div className="header-search">
+            <input className="header-searchIn"
+                type="text">   
+            </input>
+            <SearchRounded className="header-search-icon"/>
+        </div>
+
+
+        <div className="header-nav">
+            <div className="header-option">
+            </div>
+
+            <div className="header-option">
+                <span className="header-optionlineOne">
+                    Hello
+                </span>
+                <span className="header-optionlineTwo">
+                    Sign in
+                </span>
+            </div>
+          
+            <div className="header-option">
+            <span className="header-optionlineOne">
+                    Returns
+                </span>
+                <span className="header-optionlineTwo">
+                    & Orders
+                </span>
+            </div>
+          
+            <div className="header-option">
+            <span className="header-optionlineOne">
+                    Your
+                </span>
+                <span className="header-optionlineTwo">
+                    Prime
+                </span>
+            </div>
+            <div className="option-basket">
+                <ShoppingBasket/>
+                <span className="header-optionlineTwo header-count">0</span>
+            </div>
+        </div>
+    </div>
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXA7do1qBZwckF4sgNNnkALNK2m7ohfyQ",
+  authDomain: "clone-ccb16.firebaseapp.com",
+  projectId: "clone-ccb16",
+  storageBucket: "clone-ccb16.appspot.com",
+  messagingSenderId: "87335853775",
+  appId: "1:87335853775:web:48fe0d699ac7c1c72f61ba",
+  measurementId: "G-E6F7FB30PF"
+};
